47bede06ea6effad2afa57e795096c1aefcaa1d2
Rainer Sawitzki <Rainer.Sawitzki@gmail.com>
2025-07-02 10:17:01 +0200

    ToDo Slide 109

---
+++ xunit/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO BOOKS (isbn, title, price, pages) VALUES ('ISBN-1-dk', 'Python', 19.99, 400)
+INSERT INTO BOOKS (isbn, title, price, pages) VALUES ('ISBN-2-dk', 'C#', 9.99, 300)
+INSERT INTO BOOKS (isbn, title, price, pages) VALUES ('ISBN-3-dk', 'Java', 1.99, 700)
